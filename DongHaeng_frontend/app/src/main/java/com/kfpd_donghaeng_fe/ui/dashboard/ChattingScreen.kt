4f3dd6b30a823cc9b1331fd63e533b72daff13ef
Jungyeon Son <jy030210@naver.com>
2025-11-22 00:41:34 +0900

    Feat: ScheduleScreen API 연동

---
+++ DongHaeng_frontend/app/src/main/java/com/kfpd_donghaeng_fe/ui/dashboard/ChattingScreen.kt
@@ -0,0 +1,21 @@
+package com.kfpd_donghaeng_fe.ui.dashboard
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ChattingScreen() {
+    // TODO: 여기에  화면 UI 구현
+
+    // 임시 텍스트
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "채팅 화면입니다.")
+    }
+}
--- DongHaeng_frontend/app/src/main/java/com/kfpd_donghaeng_fe/ui/theme/Color.kt
+++ DongHaeng_frontend/app/src/main/java/com/kfpd_donghaeng_fe/ui/theme/Color.kt
@@ -6,11 +6,10 @@ val BrandOrange = Color(0xFFFF8216)
 val LightOrange = Color(0xFFFFE9DA)
 val MediumGray = Color(0xFFD9D9D9)
 val DarkGray = Color(0xFF888888)
+val LightGray = Color(0xFFE0E0E0)
 val TextBlack = Color(0xFF000000)
 val White = Color(0xffffffff)
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
 val Pink40 = Color(0xFF7D5260)
